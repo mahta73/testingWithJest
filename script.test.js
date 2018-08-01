63819351dc35b24fbf8d80063b1d34d290feab83
tekQueen <techandcodeaddict@gmail.com>
2018-08-01 12:07:39 +0400

    describe googleSearch

--- script.test.js
+++ script.test.js
@@ -7,11 +7,24 @@ dbMock = [
     'dogpic.com'
 ];
 
-it('is a silly test', () => {
-    expect('hello').toBe('hello');
-})
-
-it('is searching google', () => {
-    expect(googleSearch('IamTesting', dbMock)).toEqual([]);
-    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpic.com']);
+describe('googleSearch', () => {
+  
+    it('is a silly test', () => {
+        expect('hello').toBe('hello');
+    })
+    
+    it('is searching google', () => {
+        expect(googleSearch('IamTesting', dbMock)).toEqual([]);
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpic.com']);
+    })
+    
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    })
+    
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    })
+    
 })
