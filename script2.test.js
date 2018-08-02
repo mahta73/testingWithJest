8359c28d537690c164d09d31fcaef04c1cabe3de
tekQueen <techandcodeaddict@gmail.com>
2018-08-02 12:35:27 +0400

    asynchronous testing

--- script2.test.js
+++ script2.test.js
@@ -1,2 +1,30 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
+
+it ('calls swapi to get people', () => {
+    expect.assertions(1);
+    // returing a promise
+    return swapi.getPeople(fetch).then(data => {
+        expect(data.count).toEqual(87);
+    })
+})
+
+/*
+    ANOTHER WAY: 
+    it ('calls swapi to get people', (done) => {
+    expect.assertions(1);
+    return swapi.getPeople(fetch).then(data => {
+        expect(data.count).toEqual(87);
+        done();
+    })
+})
+
+*/
+it('calls swapi to get people with a promise', () => {
+    expect.assertions(2);
+    return swapi.getPeoplePromise(fetch).then(data => {
+        console.log(data.results);
+        expect(data.count).toEqual(87);
+        expect(data.results.length).toBeGreaterThan(5);
+    })
+})
