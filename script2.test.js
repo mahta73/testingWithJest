2f9f4a3fccd82caa3559a2018ba8cb4db3a5fb79
tekQueen <techandcodeaddict@gmail.com>
2018-08-05 20:49:03 +0400

    mock and spies

--- script2.test.js
+++ script2.test.js
@@ -48,8 +48,21 @@ it ('you can also use async/ await', async () => {
     expect(data.count).toBe(87);
 })
 
-/*
-it('getPeople returns count and results', () => {
-    const mockFetch = jest.fn().mockReturnValue();
+
+it('getPeoplePromise returns count and results', () => {
+    expect.assertions(4);
+
+    const mockFetch = jest.fn().mockReturnValue(Promise.resolve({
+        json: () => Promise.resolve({
+            count: 87,
+            results: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        })
+    }));
+
+    return swapi.getPeoplePromise(mockFetch).then(data => {
+        expect(mockFetch.mock.calls.length).toBe(1);
+        expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
+        expect(data.count).toEqual(87);
+        expect(data.results.length).toBeGreaterThan(5);
+    })
 })
-*/
